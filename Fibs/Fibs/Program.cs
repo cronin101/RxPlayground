7419999edead5e5ac4fc2fe2092bc191ffad35b8
Aaron Cronin <cronin@include.cat>
2015-01-29 12:22:32 +0100

    Demo of reactive Fibonacci generator

---
+++ Fibs/Fibs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+
+namespace Fibs
+{
+    using Console;
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var fibGen = Observable.Generate(
+                initialState:   new Tuple<BigInteger, BigInteger>(0, 1),
+                condition:      _   => true,
+                iterate:        tup => new Tuple<BigInteger, BigInteger>(tup.Item2, tup.Item1 + tup.Item2),
+                resultSelector: tup => tup.Item2);
+
+            var tusenFibs = fibGen.Take(1000).Select(x => x.ToString());
+
+            tusenFibs.Subscribe(WriteLine);
+
+            tusenFibs.ToTask().Wait();
+        }
+    }
+}
