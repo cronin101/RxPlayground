e1ee0f2bace4a431c90e88f13864a30b264b909d
Aaron Cronin <cronin@include.cat>
2015-01-30 17:57:56 +0100

    $ cat in FRP C#

---
+++ catsharp/catsharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace catsharp
+{
+    using Console;
+    using Observable;
+
+    static class Program
+    {
+        const string StdInFlag = "-";
+
+        static void Main(string[] args)
+        {
+            Using(OpenStandardOutput, stdOut => 
+                args.ToObservable()
+                    /* We want to mirror STDIN if no flags are given */
+                    .DefaultIfEmpty(StdInFlag)
+                    /* Monad join + Using/Return gives us resource management for free @_@ */
+                    .SelectMany(f => f.Equals(StdInFlag)
+                            ? /* Use STDIN if the alias is used */
+                            Using(OpenStandardInput, Return)
+                            : /* Otherwise, the standard FileStream specified */
+                            Using(() => File.OpenRead(f), Return)
+                                /* Sometimes, files don't exist on disk - chin up :-( */
+                                .Catch<Stream, IOException>(ex =>
+                                    Error.WriteLineAsync("cat#: " + ex.Message).ContinueWith(_=>Empty<Stream>()).Result))
+                    .Do(onNext: s => s.CopyTo(stdOut))
+             ).Wait();
+        }
+    }
+}
